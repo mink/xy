26cf69d89bac0fbcf47413f347470f8f80057247
Zac Olden <zacolden@gmail.com>
2021-02-18 01:35:26 +1100

    Configurable host & port flags

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
+var host string = "0.0.0.0"
+var port int = 8000
+
 func main() {
 	fmt.Println("xy")
 
+	flag.IntVar(&port, "port", port, "Port the HTTP server will listen on")
+	flag.StringVar(&host, "host", host, "Host the HTTP server will listen on")
+	flag.Parse()
+
 	router := mux.NewRouter()
 
-	_ = http.ListenAndServe(":8000", router)
+	addr := host + ":" + fmt.Sprint(port)
+
+	_ = http.ListenAndServe(addr, router)
 }
