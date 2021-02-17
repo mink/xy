e179190dbc5809930cec16caf9cb3600ff8f94ef
Zac Olden <zacolden@gmail.com>
2021-02-18 02:55:44 +1100

    Clean up main.go

--- main.go
+++ main.go
@@ -12,36 +12,47 @@ import (
 
 var host string = "0.0.0.0"
 var port int = 8000
+var addr string
 
 func main() {
 	fmt.Println("xy")
 
-	flag.IntVar(&port, "port", port, "Port the HTTP server will listen on")
-	flag.StringVar(&host, "host", host, "Host the HTTP server will listen on")
-	flag.Parse()
+	ParseFlags()
+	addr = CreateSocketAddress()
 
 	router := mux.NewRouter()
 
-	addr := host + ":" + fmt.Sprint(port)
-
 	fmt.Println("Server is starting...")
 
-	go NotifyServerStarted(addr)
+	go NotifyServerStarted()
 
+	StartServer(router)
+}
+
+func ParseFlags() {
+	flag.IntVar(&port, "port", port, "Port the HTTP server will listen on")
+	flag.StringVar(&host, "host", host, "Host the HTTP server will listen on")
+	flag.Parse()
+}
+
+func CreateSocketAddress() string {
+	return host + ":" + fmt.Sprint(port)
+}
+
+func NotifyServerStarted() {
+	for {
+		if _, err := net.DialTimeout("tcp", addr, time.Millisecond); err == nil {
+			break
+		}
+	}
+	fmt.Println("Server is online!")
+}
+
+func StartServer(router *mux.Router) {
 	err := http.ListenAndServe(addr, router)
 
 	if err != nil {
 		fmt.Println("The server failed to start")
 		os.Exit(1)
 	}
-}
-
-func NotifyServerStarted(addr string) {
-	for {
-		if _, err := net.DialTimeout("tcp", addr, time.Millisecond); err == nil {
-			break
-		}
-	}
-
-	fmt.Println("Server is online!")
 }
