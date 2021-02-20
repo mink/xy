663c5a1097b934118c480b054a50363164a615d0
Zac Olden <zacolden@gmail.com>
2021-02-20 16:09:25 +1100

    Check login credentials via db

--- pkg/handlers/login.go
+++ pkg/handlers/login.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"github.com/beego/beego/v2/client/orm"
+	_ "github.com/go-sql-driver/mysql"
 	"html/template"
 	"net/http"
 )
@@ -33,15 +35,39 @@ func Login(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusUnprocessableEntity)
 			data.Error = "Enter your password"
 			break
-		case password != "test":
-			w.WriteHeader(http.StatusForbidden)
-			data.Error = "Invalid credentials"
-			break
-		case password == "test":
+		default:
+			if !checkCredentials(email, password) {
+				w.WriteHeader(http.StatusForbidden)
+				data.Error = "Invalid credentials"
+				break
+			}
 			w.WriteHeader(http.StatusOK)
 			data.Error = "Success"
 		}
 	}
 
 	_ = t.Execute(w, data)
+}
+
+func checkCredentials(email string, password string) bool {
+	// temp user model
+	type User struct {
+		Email string
+		Password  int
+	}
+
+	var user User
+
+	query, _ := orm.NewQueryBuilder("mysql")
+
+	query.Select("users.email").
+		From("users").
+		Where("email = ? AND password = ?").
+		Limit(1)
+
+	orm.NewOrm().
+		Raw(query.String(), email, password).
+		QueryRow(&user)
+
+	return user.Email == email
 }
